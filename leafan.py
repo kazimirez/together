5b69d8548c949f8f4c78b7930034879d1060ce2a
Leafan <la2player_07@mail.ru>
2023-04-13 22:08:59 +0300

    v0.1.0001 добавил запись в свойство формата чертежа

--- leafan.py
+++ leafan.py
@@ -2,3 +2,15 @@
 
 def main_lfn():
     print('\nLeafan main start!')
+
+#Функция от пользователя из телеграм чатика. выводит свойства деталей
+'''def PrintPropetyValue(part) -> None:
+    property_keeper = kompas_api7_module.IPropertyKeeper(part)
+    for property_index in range(property_count):
+        property = property_mng.GetProperty(kompas_document, property_index)
+        value, base_unit, from_source = "", True, True
+        result, value, from_source = property_keeper.GetPropertyValue(property, value, base_unit, from_source)
+        print(property.Name, value)
+        if property_keeper.IsComplexPropertyValue(property):
+            value1, from_source = property_keeper.GetComplexPropertyValue(property, from_source)
+            print(property.Name, value1)'''
--- main.py
+++ main.py
@@ -34,6 +34,12 @@ propertyKeeper = api7.IPropertyKeeper(part7)
 i = 0
 for i in range (propertyMng.PropertyCount(kompas_document)):
     property = propertyMng.GetProperty(kompas_document, i)
-    propertyVal = propertyKeeper.GetPropertyValue(property, "", True, False)
-    print('\nИмя свойства: ', property.Name, '\nId свойства: ', property.Id, '\nЗначение свойства: ', propertyVal)
+    propertyVal = propertyKeeper.GetPropertyValue(property, "", True, True)
+    print('\nИмя свойства: ', property.Name, '\nId свойства: ', property.Id, '\nЗначение свойства: ', propertyVal[1])
+    if property.Name == "Форматы листов документа":
+        propertyKeeper.SetPropertyValue(property, "БЧ", True)
+        print(propertyVal[1])
+        break
     i += 1
+# Выбрать свойство
+# Установить значение
