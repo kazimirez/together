1cf57cedd2917ad01e35e237705162537bfb8d72
Leafan <la2player_07@mail.ru>
2023-04-16 17:30:40 +0300

    v0.1.0010 Добавлен класс размера

--- leafan.py
+++ leafan.py
@@ -19,8 +19,37 @@ def main_lfn():
 
 
 #Класс который описывает размер, допуски квалитеты. И возвращает строку с написанным размером для записи в Наименовании Компас 3D
-def dimension(self):
-    pass
+class Dimension:
+    def __init__(self, dim):
+        self.dim = dim
+        self.upper_deviation = None
+        self.lower_deviation = None
+        self.it_grade = None
+        self.upper_deviation_sign = "+"
+        self.lower_deviation_sign = "-"
+
+    def print_dim(self):
+        x = str(self.dim)
+        return x
+
+    def print_it_grade(self):
+        x = str(self.it_grade)
+        return x
+
+    def print_tolerance(self):
+        if self.upper_deviation == self.lower_deviation and self.upper_deviation != None:
+            x, y = str(self.upper_deviation), str(self.lower_deviation)
+            return "\u00B1" + x
+        elif self.upper_deviation != self.lower_deviation:
+            self
+            x, y = str(self.upper_deviation), str(self.lower_deviation)
+            return "$m" + x + ";" + y + "$"
+
+
+x = Dimension(23)
+x.upper_deviation = 1
+x.lower_deviation = 1.5
+print(x.print_dim(), x.print_tolerance())
 
 
 def smartRound(part_mass):
